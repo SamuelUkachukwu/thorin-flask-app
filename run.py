f8369e4eaf6b3fc9e2fd8a084d828039184d8848
SamuelUkachukwu <shybold@yahoo.com>
2022-05-14 20:25:07 +0000

    add flash message in contact.html

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 import os
 import json
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
+
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -17,6 +21,7 @@ def about():
         data = json.load(json_data)
     return render_template("about.html", page_title='About', company=data)
 
+
 @app.route("/about/<member_name>")
 def about_member(member_name):
     member = {}
@@ -30,8 +35,9 @@ def about_member(member_name):
 
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
-    if request.method == "POSt":
-        print(request.form.get(name))
+    if request.method == "POST":
+        flash("Thanks {}, we recieved your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title='Contact')
 
 
